9cec835ada8227d147606654971ef875f411240e
Nadim <nahlenadim@gmail.com>
2022-09-27 12:06:20 +0300

    Remix Create get Product

--- blog-tutorial/app/models/product.server.ts
+++ blog-tutorial/app/models/product.server.ts
@@ -7,3 +7,6 @@ export type {Product}
 export async function getProducts() {
     return prisma.product.findMany()
 }
+export async function getProduct(id: string) {
+    return prisma.product.findUnique({where : {id}});
+}
