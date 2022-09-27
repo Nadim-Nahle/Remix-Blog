20ac037b719b858b81245bb27192e3e3cb44bb9d
Nadim <nahlenadim@gmail.com>
2022-09-27 12:41:46 +0300

    Remix Get products

--- blog-tutorial/app/routes/products/index.tsx
+++ blog-tutorial/app/routes/products/index.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { getProducts } from "~/models/product.server";
+
+export const loader = async () => {
+  return json({ getProducts });
+};
 
 function Products() {
-  return <div>Products</div>;
+  const { products } = useLoaderData();
+  return (
+    <div>
+      {products.map((product: any) => (
+        <>
+          <h1>{product.title}</h1>
+          <h3>{product.description}</h3>
+        </>
+      ))}
+    </div>
+  );
 }
 
 export default Products;
